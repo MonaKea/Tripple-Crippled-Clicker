112e82f4d1c45e3a0401c4039944caa9afbbf766
MonaKea <milatrachuk@gmail.com>
2017-06-16 21:07:58 +0600

    Update Tripple_Clicker.js

--- Tripple_Clicker.js
+++ Tripple_Clicker.js
@@ -1,6 +1,8 @@
 /**
  * Created by lyudmilatrachuk on 6/16/17.
  */
+
+/**
 var click = 0;
 function increment(e){
     var x = e.clientX - 240;
@@ -12,36 +14,44 @@ function increment(e){
     }
 
 }
+ */
 
-function redraw() {
-    var canvas = document.getElementById('canvas');
-    canvas.addEventListener('click', increment);
-    if (canvas.getContext) {
-        var ctx = canvas.getContext('2d');
+var canvas = document.getElementById('canvas');
+//canvas.addEventListener('click', increment);
 
-        //the clickers' text
-        ctx.font = "20px Verdana";
-        ctx.fillText("Клики: " + click, 100, 200);
-        ctx.fillText("Клики: " + click, 270, 200);
-        ctx.fillText("Клики: " + click, 430, 200);
+if (canvas.getContext) {
+    var ctx = canvas.getContext('2d');
 
+    function redraw1() {
         //draw the rectangle
-        ctx.strokeRect(100,50,100,100);
+        ctx.strokeRect(100, 50, 100, 100);
+    }
 
+    function redraw2() {
         //draw the triangle
         ctx.beginPath();
-        ctx.moveTo(250,151);
-        ctx.lineTo(380,151);
-        ctx.lineTo(310,46);
+        ctx.moveTo(250, 151);
+        ctx.lineTo(380, 151);
+        ctx.lineTo(310, 46);
         ctx.fill();
+    }
 
+    function redraw3() {
         // draw the circle
         ctx.beginPath();
-        ctx.arc(480,100,50,0,Math.PI*2);
-        ctx.moveTo(110,75);
+        ctx.arc(480, 100, 50, 0, Math.PI * 2);
+        ctx.moveTo(110, 75);
         ctx.stroke();
     }
-}
-redraw()
 
+}
+    //ctx.font = "20px Verdana";
+    //ctx.fillText("Клики: " + click, 100, 200);
+    //ctx.fillText("Клики: " + click, 270, 200);
+    //ctx.fillText("Клики: " + click, 430, 200);
+
+
+redraw1()
+redraw2()
+redraw3()
 
