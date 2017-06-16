521c86bfa526af78261bb8d21ee2a1c290f4491d
MonaKea <milatrachuk@gmail.com>
2017-06-16 18:58:27 +0600

    Create Tripple_Clicker.js

---
+++ Tripple_Clicker.js
@@ -0,0 +1,47 @@
+/**
+ * Created by lyudmilatrachuk on 6/16/17.
+ */
+var click = 0;
+function increment(e){
+    var x = e.clientX - 240;
+    var y = e.clientY - 160;
+    var dist = Math.sqrt(y*y + x*x);
+    if (dist<100){
+        click++;
+        redraw();
+    }
+
+}
+
+function redraw() {
+    var canvas = document.getElementById('canvas');
+    canvas.addEventListener('click', increment);
+    if (canvas.getContext) {
+        var ctx = canvas.getContext('2d');
+
+        //the clickers' text
+        ctx.font = "20px Verdana";
+        ctx.fillText("Клики: " + click, 100, 200);
+        ctx.fillText("Клики: " + click, 270, 200);
+        ctx.fillText("Клики: " + click, 430, 200);
+
+        //draw the rectangle
+        ctx.strokeRect(100,50,100,100);
+
+        //draw the triangle
+        ctx.beginPath();
+        ctx.moveTo(250,151);
+        ctx.lineTo(380,151);
+        ctx.lineTo(310,46);
+        ctx.fill();
+
+        // draw the circle
+        ctx.beginPath();
+        ctx.arc(480,100,50,0,Math.PI*2);
+        ctx.moveTo(110,75);
+        ctx.stroke();
+    }
+}
+redraw()
+
+
