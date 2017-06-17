fa1e29aab56446c9c7bf63f95222272c94756679
MonaKea <milatrachuk@gmail.com>
2017-06-17 08:59:37 +0300

    three handlers for different shapes

--- Tripple_Clicker.js
+++ Tripple_Clicker.js
@@ -2,56 +2,94 @@
  * Created by lyudmilatrachuk on 6/16/17.
  */
 
-/**
-var click = 0;
-function increment(e){
-    var x = e.clientX - 240;
-    var y = e.clientY - 160;
+
+var counterRectangle = 0;
+function incrementRectangle(e){
+    var x = e.clientX - canvas.width;
+    var y = e.clientY - 0;
     var dist = Math.sqrt(y*y + x*x);
     if (dist<100){
-        click++;
+        counterRectangle++;
         redraw();
     }
 
 }
- */
 
-var canvas = document.getElementById('canvas');
-//canvas.addEventListener('click', increment);
-
-if (canvas.getContext) {
-    var ctx = canvas.getContext('2d');
-
-    function redraw1() {
-        //draw the rectangle
-        ctx.strokeRect(100, 50, 100, 100);
-    }
-
-    function redraw2() {
-        //draw the triangle
-        ctx.beginPath();
-        ctx.moveTo(250, 151);
-        ctx.lineTo(380, 151);
-        ctx.lineTo(310, 46);
-        ctx.fill();
-    }
-
-    function redraw3() {
-        // draw the circle
-        ctx.beginPath();
-        ctx.arc(480, 100, 50, 0, Math.PI * 2);
-        ctx.moveTo(110, 75);
-        ctx.stroke();
+var click2 = 0;
+function increment2(e){
+    var x = e.clientX - 0;
+    var y = e.clientY - 0;
+    var dist = Math.sqrt(y*y + x*x);
+    if (dist<100){
+        click2++;
+        redraw();
     }
 
 }
+
+var click3 = 0;
+function increment3(e){
+    var x = 330 - e.clientX;
+    var y = e.clientY + 80;
+    var dist = Math.sqrt(y*y + x*x);
+    if (dist<50){
+        click3++;
+        redraw();
+    }
+
+}
+
+
+var canvas = document.getElementById('canvas');
+canvas.addEventListener('click', incrementRectangle);
+canvas.addEventListener('click', increment2);
+canvas.addEventListener('click', increment3);
+
+var ctx = canvas.getContext('2d');
+
+function redrawRectangle() {
+    //draw the rectangle
+    ctx.strokeRect(100, 50, 100, 100);
+    ctx.font = "20px Verdana";
+    ctx.fillText("Клики: " + click1, 100, 200);
+}
+
+function redraw2() {
+    //draw the triangle
+    ctx.beginPath();
+    ctx.moveTo(250, 151);
+    ctx.lineTo(380, 151);
+    ctx.lineTo(310, 46);
+    ctx.fill();
+    ctx.font = "20px Verdana";
+    ctx.fillText("Клики: " + click2, 270, 200);
+
+}
+
+function redraw3() {
+    // draw the circle
+    ctx.beginPath();
+    ctx.arc(480, 100, 50, 0, Math.PI * 2);
+    ctx.moveTo(110, 75);
+    ctx.stroke();
+    ctx.fillText("Клики: " + click3, 430, 200);
+
+}
+
+function redraw() {
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+    redrawRectangle()
+    redraw2()
+    redraw3()
+}
+
+redraw()
+
+
     //ctx.font = "20px Verdana";
     //ctx.fillText("Клики: " + click, 100, 200);
     //ctx.fillText("Клики: " + click, 270, 200);
     //ctx.fillText("Клики: " + click, 430, 200);
 
 
-redraw1()
-redraw2()
-redraw3()
 
