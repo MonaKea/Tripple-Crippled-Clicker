11ed17218bdbee42aa27edc4a746cc5b115eb51e
MonaKea <milatrachuk@gmail.com>
2017-07-01 09:00:23 +0300

    Take parameters and make objects from them

--- Tripple_Clicker.js
+++ Tripple_Clicker.js
@@ -60,18 +60,22 @@ canvas.addEventListener('click', incrementCir2);
 
 var ctx = canvas.getContext("2d");
 
+function drawRectangle(centerX, centerY, width, height, counter) {
+    ctx.strokeRect(centerX, centerY, width, height);
+    ctx.stroke()
+    ctx.font = "20px Verdana"
+    ctx.fillText("Клики: " + counter, centerX, centerY + 150)
+}
+
+
 function redrawRect1 () {
-    ctx.strokeRect(50, 70, 100, 100);
-    ctx.font = "20px Verdana";
-    ctx.fillText("Клики: " + counterRect1, 53, 220);
+    drawRectangle(50, 70, 100, 100, counterRect1);
 }
 
 
 
 function redrawRect2 () {
-    ctx.strokeRect(350, 70, 100, 100);
-    ctx.font = "20px Verdana";
-    ctx.fillText("Клики: " + counterRect2, 355, 220);
+    drawRectangle(350, 70, 100, 100, counterRect2);
 }
 
 
@@ -83,14 +87,19 @@ function drawCircle(centerX, centerY, radius, counter) {
     ctx.fillText("Клики: " + counter, centerX - 50, centerY + radius + 50)
 }
 
-function redrawCir1() {
-    drawCircle(250, 115, 50, counterCir1)
+function drawRectangleShape(props) {
+    drawRectangle(props.x, props.y, props.w, props.h, props.counter)
+}
+function drawCircleShape(props) {
+    drawCircle(props.x, props.y, props.r, props.counter)
 }
 
-
-function redrawCir2() {
-    drawCircle(550, 115, 70, counterCir2)
-}
+var shapes = [
+    {x:50, y:70, w:100, h:100},
+    {x:350, y:70, w:100, h:100},
+    {x:250, y:115, r:50},
+    {x: 550, y: 115, r: 70},
+]
 
 function redraw() {
     incrementTotal()
@@ -98,10 +107,14 @@ function redraw() {
     ctx.stroke();
     ctx.font = "20px Verdana";
     ctx.fillText("Сумма кликов: " + counterTotal, 250, 250);
-    redrawRect1();
-    redrawRect2();
-    redrawCir1();
-    redrawCir2();
+    shapes[0].counter = counterRect1
+    drawRectangleShape(shapes[0]);
+    shapes[1].counter = counterRect2
+    drawRectangleShape(shapes[1]);
+    shapes[2].counter = counterCir1
+    drawCircleShape(shapes[2]);
+    shapes[3].counter = counterCir2
+    drawCircleShape(shapes[3]);
 }
 
 redraw();
